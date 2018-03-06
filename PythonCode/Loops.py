4ecf9f3b26cfaa41194011f26bf987548795fbb8
bhajojo <mbhalotkar@techcraftz.com>
2018-03-06 08:13:49 +0530

    Added mike Folder

--- PythonCode/Loops.py
+++ PythonCode/Loops.py
@@ -4,6 +4,7 @@ for name in Employee_Name:
     print name
 
 x=0
-for index in range( 5, 15 ,2):
-    x += 10
+for index in range(1, 16, 3):
+    x = x + 10
+    print (index)
     print ("The value of x  is {0}" .format(x))
