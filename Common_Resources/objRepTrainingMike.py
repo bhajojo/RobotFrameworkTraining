bd5e7eb5095fd28dffe54f194c9f3a5f68afc08d
bhajojo <mbhalotkar@techcraftz.com>
2018-03-11 07:25:45 +0530

    Automation script updayed

---
+++ Common_Resources/objRepTrainingMike.py
@@ -0,0 +1,11 @@
+﻿#common objects
+objLoginContinueButton="xpath=.//*[@id='wrapper']//a[contains(.,'Continue')]"
+objTopHeaderInputForm="xpath=//*[@id='navbar-brand-centered']//a[contains(.,'Input Forms')]"
+objSubHeaderHeaderSimpleDFormDemo="xpath=//*[@id='navbar-brand-centered']//a[contains(.,'Simple Form Demo')]"
+
+#Simple Input Forms
+objSubHeaderHeaderSimpleDFormDemo="xpath=//h3[contains(.,'This would be your first example to start with Selenium.')]"
+objHeadingSingleInputField="xpath=//div[@class='panel-heading' and contains(text(),'Single Input Field')]"
+objSingleInputFieldText="=xpath=//input[@id='user-message']"
+objSingleInputFieldButton="xpath=//[@id='get-input']//button"
+
